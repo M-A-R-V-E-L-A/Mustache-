f2fa33a7b61c062907178d2794fabfaf2ad5dd98
M-A-R-V-E-L-A <87486824+M-A-R-V-E-L-A@users.noreply.github.com>
2022-10-01 20:45:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX = 0;
+noseY = 0;
+
+function preload(){
+    demon_slayer = loadImage('mustache.png')
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    console.log("yoo boys we are gooinggg to runnn the vidioooo coode");
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    console.log("yoo boys itss workinnnnn");
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+ if(results.length > 0)
+ {
+    console.log(results);
+    console.log("nose x = " + noseX);
+    console.log("nose y = " + noseY);
+    noseX = results[0].pose.nose.x - 15;
+    noseY = results[0].pose.nose.y - 1;
+ }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    //fill(255,0,0);
+    //stroke(255,0,0);
+    //circle(noseX, noseY, 50);
+    image(demon_slayer, noseX, noseY, 35, 35);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
